fab1cf8942175c6ac397a53b117ff2d95ba62c52
jesseilev <jesseilev@gmail.com>
2015-05-22 17:16:07 -0500

    commit portfolio

--- src/main/java/net/cs50/finance/controllers/PortfolioController.java
+++ src/main/java/net/cs50/finance/controllers/PortfolioController.java
@@ -1,10 +1,13 @@
 package net.cs50.finance.controllers;
 
+import net.cs50.finance.models.StockHolding;
+import net.cs50.finance.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 
 /**
  * Created by Chris Bay on 5/17/15.
@@ -16,7 +19,11 @@ public class PortfolioController extends AbstractFinanceController {
     public String portfolio(HttpServletRequest request, Model model){
 
         // TODO - Implement portfolio display
+        User user = this.getUserFromSession(request);
+        Collection<StockHolding> holdings = user.getPortfolio().values();
 
+        model.addAttribute("holdings", holdings);
+        model.addAttribute("cash", user.getCash());
         model.addAttribute("title", "Portfolio");
         model.addAttribute("portfolioNavClass", "active");
 
