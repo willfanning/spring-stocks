bb89f7beb8ea483ff95653148ed905e7320c8b67
jesseilev <jesseilev@gmail.com>
2015-05-27 16:06:11 -0500

    re-implment portfolio to make less lookup calls

--- src/main/java/net/cs50/finance/controllers/PortfolioController.java
+++ src/main/java/net/cs50/finance/controllers/PortfolioController.java
@@ -1,13 +1,17 @@
 package net.cs50.finance.controllers;
 
+import net.cs50.finance.models.Stock;
 import net.cs50.finance.models.StockHolding;
+import net.cs50.finance.models.StockLookupException;
 import net.cs50.finance.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 /**
  * Created by Chris Bay on 5/17/15.
@@ -18,11 +22,47 @@ public class PortfolioController extends AbstractFinanceController {
     @RequestMapping(value = "/portfolio")
     public String portfolio(HttpServletRequest request, Model model){
 
-        // TODO - Implement portfolio display
         User user = this.getUserFromSession(request);
+
+        // assemble portfolio data to pass to template
+
+        ArrayList<HashMap<String, String>> portfolioData = new ArrayList<HashMap<String, String>>();
+
         Collection<StockHolding> holdings = user.getPortfolio().values();
 
-        model.addAttribute("holdings", holdings);
+        // for each holding the user has, add an entry to portfolio data
+        for (StockHolding holding : holdings) {
+
+            // assemble a hashmap with the data for this holding
+
+            HashMap<String, String> holdingData = new HashMap<String, String>();
+
+            // add symbol and shares to the holding data
+            holdingData.put("symbol", holding.getSymbol());
+            holdingData.put("shares", String.valueOf(holding.getSharesOwned()));
+
+            // lookup current stock info
+            Stock currentStock = null;
+            try {
+                currentStock = Stock.lookupStock(holding.getSymbol());
+            } catch (StockLookupException e) {
+                e.printStackTrace();
+                return this.displayError("Unable to display portfolio", model);
+            }
+
+            // add price to the holding data
+            String priceDisplay = "$" + String.format("%.2f", currentStock.getPrice());
+            holdingData.put("price", priceDisplay);
+
+            // add total value to the holding data
+            float totalValue = currentStock.getPrice() * holding.getSharesOwned();
+            String totalValueDisplay = "$" + String.format("%.2f", totalValue);
+            holdingData.put("total_value", totalValueDisplay);
+
+            portfolioData.add(holdingData);
+        }
+
+        model.addAttribute("portfolioData", portfolioData);
         model.addAttribute("cash", user.getCash());
         model.addAttribute("title", "Portfolio");
         model.addAttribute("portfolioNavClass", "active");
